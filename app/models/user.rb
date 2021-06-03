866905e8a37e6262dadabe7ac6f846c9a6625e39
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-06-03 05:40:27 +0000

    Add フォロー/フォロワー

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,22 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness:true, length: { minimum: 2, maximum: 20}
   validates :introduction, length: { maximum: 50 }
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   root to:'homes#top'
 
   get '/home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
